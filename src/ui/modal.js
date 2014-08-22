84b313555b6bfa843f8e2c3d2d9625bd5dfc039e
wuhf <greedisgood5000@163.com>
2014-08-22 16:29:43 +0800

    弹出框升级

--- src/ui/modal.js
+++ src/ui/modal.js
@@ -1,14 +1,24 @@
 (function ($) {
-    var $backdrop;
+
+    var animate = function($elem, animateArgs){
+        return ($.fn.transit || $.fn.animate).apply($elem, animateArgs);
+    }
+    var openCauseClose;
     /**
-     * µ¯³ö¿ò
+     * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
      * @param {jQuery.Deferred|jQuery|function|string} content
      * @param {object} options
      * @constructor
      */
-    var Modal = function(content, options){
-        if (!(this instanceof Modal)) return new Modal(content, options);
-        this.options = $.extend({}, arguments.callee.defaults);
+    var Dialog = function(content, options){
+        var callee = arguments.callee;
+        if (!(this instanceof callee)) return new callee(content, options);
+        this.options = $.extend({}, callee.defaults, options);
+        callee.factory.call(this, content, options);
+        this.constructor = callee;
+    };
+    Dialog.factory = function(content){
+        var that = this;
         if (typeof content == 'string' || /\//.test(content)) {
             var selector, url, off = content.indexOf(" ");
             if ( off >= 0 ) {
@@ -31,112 +41,223 @@
             };
         else
             this._init = content;
-        this.$element = $('<div class="modal" style="position: absolute; z-index:1001; display: none; overflow: hidden;"></div>');
-    };
-    Modal.list = [];
-    Modal.toogleBackDrop = function(toggle){
-        toggle = toggle == null ? $backdrop.css('display') == 'none' : !!toggle;
-
-        $('body').toggleClass('with-backdrop', toggle);
-        if (!$backdrop) $backdrop = $('<div class="backdrop" style="display: none;"></div>').prependTo('body').bgiframe();
-        var opacity = $backdrop.css('opacity');
-
-        if (toggle) {
-            $backdrop.css({
-                display: 'block',
-                opacity: 0
-            }).transit({
-                opacity: opacity
-            }, function(){
-                $(this).css({opacity: ''})
-            })
-        } else {
-            $backdrop.css({
-                opacity: opacity
-            }).transit({
-                opacity: 0
-            }, function(){
-                $(this).css({opacity: '', display: 'none'})
-            })
-        }
+        this.$element = $('<div class="modal" tabindex="1" style="position: absolute; z-index:1001; display: none; overflow: hidden;"></div>');
     }
-    Modal.prototype = {
-        constructor: Modal,
+    Dialog.toogleBackDrop = function(toggle, $backdrop){
+        $backdrop = $backdrop || this.defaults.backdrop;
+        if (!$backdrop) return;
+        var $body = $('body');
+        if (!$.contains($body[0], $backdrop[0])) {
+            $backdrop.prependTo('body').bgiframe();
+        }
+        toggle = toggle == null ? $backdrop.css('display') == 'none' : !!toggle;
+        $body.toggleClass('with-backdrop', toggle);
+        animate($backdrop, [{
+            opacity: toggle ? 'show' : 'hide'
+        }]);
+    }
+    Dialog.closeAll = function(list, returnValue, closeOptions){
+        list = list || this.defaults.queue;
+        var $backdrop;
+        list.forEach(function(item){
+            var co = $.extend(closeOptions, item.options.close);
+            var rt = returnValue || co.returnValue;
+            rt = $.isFunction(rt) ? rt.call(this) : rt;
+            $backdrop = item.options.backdrop;
+            item._close(rt, co)
+        });
+        list.length = 0;
+        !openCauseClose && $backdrop && this.toogleBackDrop(false, $backdrop);
+    }
+    Dialog.prototype = $.EventEmitter({
         init: function(){
-            var e = $.Event('init'), dfd, self = this;
-            self.$element.trigger(e);
-            if (!e.isDefaultPrevented()) {
-                dfd = self._init();
-            } else dfd = $.Deferred.reject();
-            dfd.done(function($elem){
-                self._init = dfd;
-                self.$element.append($elem.show()).appendTo('body');
-            })
-        },
-        _open: function(){
             var self = this;
-            if (Modal.list.indexOf(self) >= 0) return;
-            if (!Modal.list.length) {
-                Modal.toogleBackDrop(true);
-                Modal.list.push(self);
-            }
-            else {
-                Modal.list.push(self);
-                Modal.list[0].close();
-            }
-            self.$element.css({
-                display: 'block',
-                opacity: 0
-            }).position({of:$(window), at:'center center-50', my:'center center'}).finish().transit({
-                top: '+=50',
-                opacity: '1'
-            }, function(){
-                self.$element.find('.modal-form :text, .modal-form textarea').eq(0).focus();
-            })
+            if (typeof this._init == "function") {
+                return this._init().done(function($elem){
+                    self._init = this;
+                    self.$element.append($elem.show()).appendTo('body');
+                })
+            } else return this._init
         },
-        _close: function(){
-            if (!(Modal.list.indexOf(this) >= 0)) return;
-            $.Array.remove(Modal.list, this);
-            if (!Modal.list.length) Modal.toogleBackDrop(false);
+        focus: function(){
+            var list = this.options.queue;
+            var z = this.options.zIndex;
+            var that = this;
+            var thisZindex;
+            $.Array.remove(list, that);
+            list.push(that);
+            list.forEach(function(item, i){
+                var s = z.start + i * z.step;
+                if (item == that) thisZindex = s
+                item.$element.css('zIndex', s);
+            })
+            this.options.backdrop && this.options.backdrop.css('zIndex', thisZindex);
 
-            this.$element.finish().transit({
-                opacity: 0,
-                top: '-=50'
-            }, function(){
-                this.style.display = 'none';
-                this.style.top = ''
-                this.style.left = ''
+        },
+        _open: function(openOptions){
+            var list = this.options.queue, self = this, index, position;
+            if (list.indexOf(self) >= 0) return;
+            self.$element.on('focus.dialog', function(e){
+                self.trigger(e);
             });
-
+            if (!list.length) {
+                if (openOptions.showBackdrop)
+                    this.constructor.toogleBackDrop(true, this.options.backdrop);
+            } else {
+                openCauseClose = true;
+                if (openOptions.closeOthers) {
+                    this.constructor.closeAll();
+                }
+                openCauseClose = false;
+            }
+            list.push(self);
+            position = typeof openOptions.position == 'object' ? openOptions.position : openOptions.position(list.indexOf(self));
+            position.of = position.of || this.options.attach;
+            self.$element.finish().position(position);
+            return animate(self.$element, openOptions.animate).promise().done(function(){
+                self.trigger('opened');
+            });
         },
-        close: function(){
-            var e = $.Event('close'), self = this;
-            this.$element.trigger(e);
-            if (!e.isDefaultPrevented()) {
-                self._close();
+        _close: function(returnValue, closeOptions){
+            var self = this;
+            self.$element.off('focus.dialog');
+            return animate(this.$element, closeOptions.animate).promise().done(function(){
+                this[0].style.top = '';
+                this[0].style.left = '';
+                self.trigger('closed', [returnValue]);
+            });
+        },
+        close: function(returnValue, closeOptions){
+            var self = this, list = this.options.queue;
+            if (!(list.indexOf(this) >= 0)) return;
+            closeOptions = $.extend({}, this.options.close, closeOptions);
+            returnValue = returnValue || closeOptions.returnValue;
+            returnValue = $.isFunction(returnValue) ? returnValue.call(this) : returnValue;
+            this._close(returnValue, closeOptions);
+            $.Array.remove(this.options.queue, this);
+            if (!openCauseClose) {
+                if (!list.length) this.constructor.toogleBackDrop(false, this.options.backdrop);
+                list.length && list[list.length - 1].$element.trigger('focus');
             }
         },
-        open: function(dfd){
+        open: function(dfd, openOptions){
             var self = this;
-            $.when(typeof self._init == "function" ? self.init() : self._init, dfd).done(function(){
-                var e = $.Event('open');
-                self.$element.trigger(e);
-                if (!e.isDefaultPrevented()) {
-                    self._open();
-                }
+            if (!$.isDeferred(dfd)) {
+                openOptions = dfd;
+                dfd = null;
+            }
+            openOptions = $.extend({}, this.options.open, openOptions);
+            dfd = dfd || openOptions.dfd;
+            var init;
+            if (typeof this._init == 'function') {
+                var e = $.Event('init');
+                self.trigger(e);
+                if (!e.isDefaultPrevented())
+                    init = e.actionReturns
+                else init = $.Deferred.reject()
+            } else
+                init = self._init;
+            $.when(init, dfd).done(function(){
+                self._open(openOptions);
+                if (openOptions.getFocus)
+                    self.$element.trigger('focus')
+                else
+                    self.trigger('focus');
             })
         }
-    };
+    });
 
-    Modal.defaults = {};
+
+    Dialog.defaults = {
+        queue: [],
+        attach: $(window),
+        zIndex: {
+            start: 100,
+            step: 10,
+            end: 300
+        },
+        open: {
+            position: {
+                at: 'left' + ' bottom' + '+15',
+                my: 'left top'
+            },
+            showBackdrop: false,
+            closeOthers: true,
+            getFocus: false,
+            animate: [{
+                top: '-=10',
+                opacity: 'show'
+            }]
+        },
+        close: {
+            animate: [{
+                opacity: 'hide',
+                top: '+=10'
+            }]
+        },
+        onopen: $.noop,
+        onopened: $.noop,
+        onclose: $.noop,
+        onclosed: $.noop,
+        oninit: $.noop,
+        onfocus: $.noop
+    };
 
     $.fn.modal = function(options){
         var self = this[0], modal;
         if (modal = $.data(self, 'ui-modal'))
             return modal;
-        else $.data(self, 'ui-modal', Modal(self, options));
+        else $.data(self, 'ui-modal', $.UI.Modal(self, options));
         return this;
     }
-    $.UI.Modal = Modal;
+    $.UI.Dialog = Dialog;
+    $.UI.Modal = $.Function.clone(Dialog);
+    $.UI.Modal.defaults = {
+        queue: [],
+        attach: $(window),
+        backdrop: $('<div class="backdrop" style="display: none;"></div>'),
+        zIndex: {
+            start: 1100,
+            step: 10,
+            end: 1300
+        },
+        open: {
+            position: function(index){
+                var stepX = 20;
+                var stepY = 20;
+                var offestX = index * stepX;
+                var offestY = index * stepX - 30;
+                offestX = offestX ? offestX.toString() : '';
+                offestY = offestY ? offestY.toString() : '';
+                return {
+                    at: 'center' + offestY + ' center' + offestY,
+                    my: 'center center'
+                }
+            },
+            showBackdrop: true,
+            closeOthers: true,
+            getFocus: true,
+            animate: [{
+                top: '+=30',
+                opacity: 'show'
+            },{
+                done: function(){
+                    $(this).find('.modal-form :text, .modal-form textarea').eq(0).focus();
+                }
+            }]
+        },
+        close: {
+            animate: [{
+                opacity: 'hide',
+                top: '+=30'
+            }]
+        },
+        onopen: $.noop,
+        onopened: $.noop,
+        onclose: $.noop,
+        onclosed: $.noop,
+        oninit: $.noop,
+        onfocus: $.noop
+    }
 
 })(jQuery);
